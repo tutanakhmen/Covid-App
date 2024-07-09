96fbc94ddcffc4051ee9fee625627c291510896b
Hasan Albana <hasanbena02@gmail.com>
2024-07-09 15:01:21 +0700

    fix: error on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,11 @@
-import Form from "../components/Form/Form";
 import Hero from "../components/Hero/Hero";
-import Indonesia from "../components/Indonesia/Indonesia";
-import Provinsi from "../components/Provinsi/provinsi";
 
 
 function Home() {
   return (
     <div>
       <Hero />
-      <Indonesia />
-      <Provinsi />
-      <Form />
+      
     </div>
   );
 }
